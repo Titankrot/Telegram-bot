d7a1d4e50634cae4257ccefcf07a1a667191b5cb
Ivan <ivan912ivan@gmail.com>
2019-09-29 16:13:16 +0500

    Authorization
    Добавили возможность авторизации, вывод данных через /info

--- src/Bot.java
+++ src/Bot.java
@@ -1,13 +1,14 @@
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.User;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.HashMap;
 
 public class Bot extends TelegramLongPollingBot {
-    private HashMap<Long, Student> Students = new HashMap<>();
+    private HashMap<Integer, Student> Students = new HashMap<>();
 
     private void sendText(Message message, String str){
         SendMessage sendMessage = new SendMessage();
@@ -24,16 +25,33 @@ public class Bot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
-        Long id = message.getChatId();
-        if (!Students.containsKey(id))
-            Students.put(id, new Student(id));
-        if (message != null && message.hasText()){
+        int id = message.getFrom().getId();
+        if (!Students.containsKey(id)) {
+            Student student = new Student(id);
+            Students.put(id, student);
+            sendText(message, student.askAthorizationQues(0));
+            return;
+        }
+        Student student = Students.get(id);
+        if (student.isNotAuthorized() &&
+                message.getText() != null &&
+                message.getReplyToMessage() != null &&
+                Student.QuestionsToAttr.containsKey(message.getReplyToMessage().getText())) {
+            String question = message.getReplyToMessage().getText();
+            student.setAttr(Student.QuestionsToAttr.get(question), message.getText());
+            sendText(message, student.askAthorizationQues(Student.questionNumber(question) + 1));
+            return;
+        }
+        if (message.hasText()){
             switch (message.getText()){
                 case "/help":
                     sendText(message, "hz poka");
                     break;
                 case "/settings":
                     sendText(message, "hz poka");
+                    break;
+                case "/info":
+                    sendText(message, student.ToString());
                     break;
                 default:
                     sendText(message, "hz poka");
--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,45 @@
+import java.util.Map;
+
 public class Student {
-    private Long Id;
+    private int Id;
     private String Name;
     private String Group;
     private String SubGroup;
 
-    public Student(Long id) { this.Id = id; }
+    Student(int id) { this.Id = id; }
+
+    private static String[] AuthorizeQuestions = new String[]{
+            "Как твое имя?",
+            "Какая у тебя группа?",
+            "Какой номер твоей группы?"
+    };
+
+    static int questionNumber(String quest){
+        for (int i = 0; i < AuthorizeQuestions.length; i++) {
+            String p = AuthorizeQuestions[i];
+            if (AuthorizeQuestions[i].equals(quest))
+                return i;
+        }
+        return -1;
+    }
+
+    static Map<String, String> QuestionsToAttr = Map.of(
+            AuthorizeQuestions[0], "Name",
+            AuthorizeQuestions[1], "Group",
+            AuthorizeQuestions[2], "SubGroup"
+    );
+    
+    boolean isNotAuthorized(){
+        for (String value: new String[]{this.Name, this.Group, this.SubGroup}) {
+            if (value == null)
+                return true;
+        }
+        return false;
+    }
+
+    String askAthorizationQues(int authorizeStep){
+        return AuthorizeQuestions[authorizeStep];
+    }
 
     public String getName() { return  this.Name; }
 
@@ -17,4 +52,25 @@ public class Student {
     public String getSubGroup() { return this.SubGroup; }
 
     public void setSubGroup(String value) { this.SubGroup = value; }
+
+    void setAttr(String attrName, String value){
+        switch (attrName){
+            case "Name":
+                this.Name = value;
+                break;
+            case "Group":
+                this.Group = value;
+                break;
+            case "SubGroup":
+                this.SubGroup = value;
+                break;
+        }
+    }
+
+    String ToString(){
+        if (this.isNotAuthorized())
+            return "Пользователь не авторизован";
+        return String.format("Имя: %s\nГруппа: %s\nНомер группы: %s",
+                this.Name, this.Group, this.SubGroup);
+    }
 }
